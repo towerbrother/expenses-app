81184ae1583048fbf6992014936af03221c5b7d8
Giorgio Torre <giorgio.torre8@gmail.com>
2020-07-21 21:17:13 +0100

    Installed express.js and removed help page route

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -15,7 +15,6 @@ const AppRouter = () => (
         <Route path="/" component={ExpenseDashboardPage} exact={true} />
         <Route path="/create" component={AddExpensePage} />
         <Route path="/edit/:id" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
