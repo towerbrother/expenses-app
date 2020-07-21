4a2d8ff2cd9f0dd4c422391ec9338cc66bae3ad2
Giorgio Torre <giorgio.torre8@gmail.com>
2020-07-21 21:23:29 +0100

    Modified Header component and AppRouter, created server.js file

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,6 @@ const Header = () => (
     <NavLink to="/create" activeClassName="is-active">
       Add expense
     </NavLink>
-    <NavLink to="/help" activeClassName="is-active">
-      Help
-    </NavLink>
   </header>
 );
 
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,7 +4,6 @@ import AddExpensePage from "./../components/AddExpensePage";
 import EditExpensePage from "./../components/EditExpensePage";
 import ExpenseDashboardPage from "./../components/ExpenseDashboardPage";
 import Header from "./../components/Header";
-import HelpPage from "./../components/HelpPage";
 import NotFoundPage from "./../components/NotFoundPage";
 
 const AppRouter = () => (
