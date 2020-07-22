e691c728182ffc34c68914466c34fb540ceea69d
Giorgio Torre <giorgio.torre8@gmail.com>
2020-07-22 07:12:08 +0100

    Changed script start to set up Heroku

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const port = 3000;
 const app = express();
-const publicPath = path.join(__dirname, "..", "public");
+const publicPath = path.join(__dirname, "..", "build");
 
 app.use(express.static(publicPath));
 
