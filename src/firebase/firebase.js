7c25cf3178042234d8065ddfda7aa1214f8f0a58
Giorgio Torre <giorgio.torre8@gmail.com>
2020-07-25 12:56:48 +0100

    Setup Firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,27 @@
+import * as firebase from "firebase/app";
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA026SwOqf7PqGHa2nF7_Fi__6kgqpkd-g",
+  authDomain: "tower-budget-app.firebaseapp.com",
+  databaseURL: "https://tower-budget-app.firebaseio.com",
+  projectId: "tower-budget-app",
+  storageBucket: "tower-budget-app.appspot.com",
+  messagingSenderId: "31373530907",
+  appId: "1:31373530907:web:8d790c1077d1d90ba265be",
+  measurementId: "G-6GDY7X9ERN",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+firebase
+  .database()
+  .ref("users")
+  .set({
+    name: "Giorgio Torre",
+    age: 30,
+    location: {
+      city: "London",
+      country: "United Kingdom",
+    },
+  });
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import "normalize.css/normalize.css"; // css reset
 import "./styles/styles.scss";
 import "react-dates/initialize";
 import "react-dates/lib/css/_datepicker.css";
+import "./firebase/firebase";
 
 const store = configureStore();
 
