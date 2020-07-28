ff094c34f01f61c67c10b656a5f809b5cee96014
Giorgio Torre <giorgio.torre8@gmail.com>
2020-07-28 12:13:38 +0100

    Deleted playground folder and created .env.development and .env.test to ensure that two separate firebase database are used depending on whether we are on development or test

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,15 +3,25 @@ import firebase from "firebase/app";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA026SwOqf7PqGHa2nF7_Fi__6kgqpkd-g",
-  authDomain: "tower-budget-app.firebaseapp.com",
-  databaseURL: "https://tower-budget-app.firebaseio.com",
-  projectId: "tower-budget-app",
-  storageBucket: "tower-budget-app.appspot.com",
-  messagingSenderId: "31373530907",
-  appId: "1:31373530907:web:8d790c1077d1d90ba265be",
-  measurementId: "G-6GDY7X9ERN",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA026SwOqf7PqGHa2nF7_Fi__6kgqpkd-g",
+//   authDomain: "tower-budget-app.firebaseapp.com",
+//   databaseURL: "https://tower-budget-app.firebaseio.com",
+//   projectId: "tower-budget-app",
+//   storageBucket: "tower-budget-app.appspot.com",
+//   messagingSenderId: "31373530907",
+//   appId: "1:31373530907:web:8d790c1077d1d90ba265be",
+//   measurementId: "G-6GDY7X9ERN",
+// };
 
 firebase.initializeApp(firebaseConfig);
 
