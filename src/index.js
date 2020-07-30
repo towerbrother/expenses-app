3b41221dde9481dd81235adcfbd699d34e8f7513
Giorgio Torre <giorgio.torre8@gmail.com>
2020-07-30 18:12:53 +0100

    Removed debugging console.log() in index.js

--- src/index.js
+++ src/index.js
@@ -36,12 +36,10 @@ firebase.auth().onAuthStateChanged((user) => {
       if (history.location.pathname === "/") {
         history.push("/dashboard");
       }
-      console.log("login");
     });
   } else {
     store.dispatch(logout());
     renderApp();
     history.push("/");
-    console.log("logout");
   }
 });
